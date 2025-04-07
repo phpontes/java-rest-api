4b94797a1e65f58763ddf6435777a4a53d62b72e
phpontes <ppontes95@hotmail.com>
2025-04-07 15:28:57 -0300

    Enhance validation and error handling: add validation annotations to Contact model, update exception handling in ApplicationExceptionHandler, and modify ErrorResponse to handle list of messages.

--- src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
+++ src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
@@ -1,19 +1,39 @@
 package com.ltp.contacts;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import exception.ContactNotFoundException;
 import exception.ErrorResponse;
 
 @ControllerAdvice
-public class ApplicationExceptionHandler {
-    
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(ContactNotFoundException.class)
     public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+                List<String> errors = new ArrayList<>();
+                for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+                    errors.add(error.getDefaultMessage());
+                }
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/exception/ErrorResponse.java
+++ src/main/java/exception/ErrorResponse.java
@@ -1,6 +1,7 @@
 package exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -8,18 +9,18 @@ public class ErrorResponse {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
-    private String message;
+    private List<String> message;
 
-    public ErrorResponse(String message) {
+    public ErrorResponse(List<String> message) {
         timestamp = LocalDateTime.now();
         this.message = message;
     }
 
-    public String getMessage() {
+    public List<String> getMessage() {
         return this.message;
     }
 
-    public void setMessage(String message) {
+    public void setMessage(List<String> message) {
         this.message = message;
     }
 
